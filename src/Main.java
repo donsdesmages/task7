9c7aa5606d6defd48d12d6ee61077247db99384d
vladimir <snigereff.v@yandex.ru>
2023-02-02 22:39:02 +0300

    homework

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Set<Integer> integerSet = new HashSet<>();
+
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(1);
+        integerList.add(1);
+        integerList.add(1);
+        integerList.add(2);
+        integerList.add(3);
+        integerList.add(4);
+        integerList.add(5);
+        integerList.add(5);
+        integerList.add(6);
+
+        /* integerList.forEach(integer -> System.out.println(integerList));*/
+
+        for (int i = 0; i < integerList.size() - 1; i++) {
+            if (integerList.get(i) != integerList.size()) {
+                if (integerList.get(i)!= 3 && integerList.get(i)!= 4 ) {
+                    integerList.remove(integerList.get(i));
+                }
+            }
+        }
+        System.out.println(integerList);
+    }
+}
